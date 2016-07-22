5eff6280eaa39782239a0032df7da6863ff9ada0
sophiallen <smlelevich@gmail.com>
2016-07-22 12:16:26 -0700

    converted to use express

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-//function to set headers and send data flexibly
-function serveStatic(res, path, contentType, responseCode){
-	if (!responseCode){
-		//set response code to 200 unless otherwise specified
-		responseCode = 200;
-	}
+var app = new express();
 
-	fs.readFile(__dirname + path, function(err, data){
-		if(err){ 
-			//handle server errors
-			res.writeHead(500, {'Content-Type': 'text/plain'});
-			res.end('500 - Internal Error');
-		} else {
-			//set appropriate header, send data.
-			res.writeHead(responseCode, {'Content-Type': contentType});
-			res.end(data);
-		}
-	});
-}
+//use environment port or default to port 3000
+app.set('port', process.env.PORT || 3000);
 
-//create server and handle basic routes
-http.createServer(function(req, res){
-	var path = req.url.toLowerCase();
-	switch(path){
-		case '/':
-			//send html file for home page
-			serveStatic(res, '/public/home.html', 'text/html');
-			break;
-		case '/about':
-			serveStatic(res, '/public/about.html', 'text/html');
-			break;
-		default: 
-			//if not home or about, send 404 response.
-			res.writeHead(404, {'Content-Type' : 'text/plain'});
-			res.end('404: Page not found.');
-	}
-})
-.listen(process.env.PORT || 3000);
+//use public folder to serve static files
+app.use(express.static('public'));
 
-console.log("Server started, listening on port 3000");
+//body parser for url query strings
+app.use(bodyParser.urlencoded({extended: true}));
+
+//home page
+app.get('/', function(req, res){
+	res.setHeader('Content-Type', 'text/html');
+	var options = {root: __dirname + '/public'};
+	res.status(200).sendFile('home.html', options);
+});
+
+//about page
+app.get('/about', function(req, res){
+	res.type('text/html');
+	var options = {root: __dirname + '/public'};
+	res.status(200).sendFile('about.html', options);
+});
+
+//handle 404 errors
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404).send('404 - file not found :(');
+});
+
+//start server
+app.listen(app.get('port'), function(){
+	console.log('express server started');
+});
+
+
+
+
