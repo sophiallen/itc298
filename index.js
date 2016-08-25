15e7009fc40872653bb8347b60c6d1e66c861d1c
sophiallen <smlelevich@gmail.com>
2016-08-24 18:18:32 -0700

    moved db initialization to models file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
-var keys = require('./keys.js');
+// var keys = require('./keys.js');
 
 var app = new express();
 
@@ -14,15 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api', require('cors')());
 
-//connect to mongodb
-var connectionString = 'mongodb://' + keys.dbUserName + ':' + keys.dbPass + keys.dbUrl;
-mongoose.connect(connectionString);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log("successfully connected to mongodb");
-});
 
 //Routes
 var api = require('./routes/api.js')(app);
--- models/language.server.models.js
+++ models/language.server.models.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var keys = require('./../keys.js');
+
+//connect to mongodb
+var connectionString = 'mongodb://' + keys.dbUserName + ':' + keys.dbPass + keys.dbUrl;
+mongoose.connect(connectionString);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("successfully connected to mLab mongodb.");
+});
 
 var languageSchema = new Schema({
 	name: String,
